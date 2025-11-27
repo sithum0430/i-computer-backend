548b60f7a546292c766dcf91f803bbfc0abab888
sithum0430 <e21287@eng.pdn.ac.lk>
2025-11-27 06:42:39 +0530

    add functions create products and get products and connect the routes

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,1 +1,42 @@
-import Product from "../models/product";
+import Product from "../models/product.js";
+
+
+export function createProduct(req, res) {
+
+    if(!isAdmin(req)){// 403 error ekak kiyanne  Forbidden Error happens when the web page (or another resource) that you're trying to open in your web browser is a resource that you're not allowed to access
+        res.status(403).json({
+            message: "Admin access required to add products"
+        });
+        return;
+    }
+
+    const newProductData = req.body;
+
+    const product = new Product(newProductData)
+
+    product.save().then(
+        ()=>{
+            res.json({
+                message: "Product created successfully"
+            })
+        }).catch((error)=>{
+            res.status(500).json({
+                message: error // error ela frontend ekata yawanna  
+            })
+            console.log(error);// error ekka backend terminal eke print karaganna 
+        })
+  
+}
+
+export function getProducts(req, res) {
+    Product.find().then(
+        (products)=>{
+            res.json(products);
+        })
+    }
+
+
+
+
+
+
--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import mongoose from 'mongoose';
 import userRouter from './routes/userRouter.js';
 import authoraizeUser from './lib/jwtMiddlewear.js';
 import dotenv from 'dotenv';
+import productRouter from './routes/productRouter.js';
+
 dotenv.config();
 
 const mongoURI = process.env.MONGO_DB_URI // mongoDb eken link eka aran ekat password eka dagatha
@@ -19,12 +21,13 @@ mongoose.connect(mongoURI).then(
 
 const app = express();// complete backend software ekak app kiyana eka athulata dagatha
 app.use(express.json()); // meken wenne middlewear create wenawa apahadili data pahadili karala denawa json format eken
-app.use (authoraizeUser)
+app.use (authoraizeUser);
 
 
 
 
 app.use("/users", userRouter);
+app.use("/products", productRouter);
 
 
 
